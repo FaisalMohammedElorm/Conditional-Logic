2c0c41aeb9ed3bb6a155616f4e2b8d284a2336e5
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-15 20:23:08 +0000

    number check

--- Checker.js
+++ Checker.js
@@ -6,3 +6,11 @@ if (number % 2 === 0){
 else {
   console.log("Number is odd");
 }
+// Check for leap year
+const year = 2024;
+if ( year % 4 === 0 && year % 100 !==0){
+  console.log("Is a leap year");
+}
+else {
+  console.log("Is not a leap year");
+}
