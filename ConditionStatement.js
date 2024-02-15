f6471695b4e3e9c267b715d11a84985e798344bc
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-15 20:07:03 +0000

    condition

---
+++ ConditionStatement.js
@@ -0,0 +1,9 @@
+const age = 28;
+if (age === 18 ) {
+  console.log("You can vote one person");
+} else if (age > 18){
+  console.log("You can vote two people");
+}
+else {
+  console.log("You can't vote");
+}
