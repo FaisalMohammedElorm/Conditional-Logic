dddf4be826180134eca7073e26bc0128c3e48f22
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-17 21:06:33 +0000

    loop

--- loops.js
+++ loops.js
@@ -9,16 +9,34 @@
 
 
 // for loop
-for(let i = 0; i <= 11; i++){
+// for(let i = 0; i <= 11; i++){
   // code to be executed
-  console.log(i);
-}
+  // console.log(i);
+// }
 // while loop
 
-let i = 0;
-while (i <= 10){
-  console.log(i);
-  if (i === 5)
-  break;
-i++
+// let i = 0;
+// while (i <= 10){
+//   console.log(i);
+//   if (i === 5)
+//   break;
+// i++
+// }
+// do while loop
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+
+// }
+// while (i <= 10);
+// for in loop
+const personDetails = {
+  name: "Faisal",
+  age: 20,
+  gender: "Male",
+
+}
+for (let key in personDetails){
+  console.log(key, personDetails[key]);
 }
