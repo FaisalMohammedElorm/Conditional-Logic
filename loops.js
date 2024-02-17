997101cbed1b6974c153ccbd3c1ab42ffd1d04a3
Mohammed Faisal <mohammedelormf@gmail.com>
2024-02-17 20:53:06 +0000

    loop

---
+++ loops.js
@@ -0,0 +1,24 @@
+// for loop
+// while loop
+// do while loop
+// for in loop
+// for of loop
+// map
+// forEach 
+
+
+
+// for loop
+for(let i = 0; i <= 11; i++){
+  // code to be executed
+  console.log(i);
+}
+// while loop
+
+let i = 0;
+while (i <= 10){
+  console.log(i);
+  if (i === 5)
+  break;
+i++
+}
